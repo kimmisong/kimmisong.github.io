2d4324036d509a79e33af81929c1302d9da51057
kimmisong <sarin3@naver.com>
2017-05-19 12:25:31 +0900

    오류집기

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
- var init = functon(){
+ var init = function(){
    var sec = document.getElementById('sec');
-   var min = document.getElementById('min');
    var min = document.getElementById('min');
    var hour = document.getElementById('hour');
 
    var time;
 
-   functon intervalHandler() {
+   function intervalHandler() {
      time = new Date();
      sec.innrHTML = time.getSeconds();
      min.innrHTML = time.getMinutes();
      hour.innrHTML = time.getHours();
    }
-   intervalHandler()
+
+   setInterval(intervalHandler, 1000);
 
  };
 
